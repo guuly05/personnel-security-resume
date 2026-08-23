4717dafd97ec6fc1fa28a6c05d8983efd721a41a
guuly05 <guuleedmaxamuud40@gmail.com>
2026-08-23 22:29:20 +0300

    perf: remove global speed insights load

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
-import { SpeedInsights } from '@vercel/speed-insights/react';
 import App from './App.tsx';
 import './index.css';
 
@@ -9,7 +8,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
       <App />
-      <SpeedInsights />
     </HelmetProvider>
   </StrictMode>,
 );
