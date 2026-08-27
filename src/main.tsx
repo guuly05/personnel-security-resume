906285c57a434606ed51336acc463b700e44c260
guuly05 <guuleedmaxamuud40@gmail.com>
2026-08-27 12:18:52 +0300

    update: src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
+import { MotionConfig } from 'motion/react';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
-      <App />
+      <MotionConfig reducedMotion="user">
+        <App />
+      </MotionConfig>
     </HelmetProvider>
   </StrictMode>,
 );
